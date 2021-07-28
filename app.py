e8b61ab1b9c1878ce31bda0cc75121ec6e5fec98
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-07-28 12:53:43 -0500

    adds git init just in case

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
-db=SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
-@app.route('/', methods=['POST','GET'])
-
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
@@ -30,13 +30,13 @@ def index():
             return "There was an issue adding your task"
 
     else:
-        tasks= Todo.query.order_by(Todo.date_created).all()
+        tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
-@app.route('/delete/<int:id>')
 
+@app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete=Todo.query.get_or_404(id)
+    task_to_delete = Todo.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
@@ -45,13 +45,13 @@ def delete(id):
     except:
         return "There was a problem deleting that task"
 
-@app.route('/update/<int:id>',methods=['GET','POST'])
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
-    if request.method=='POST':
-        task.content=request.form['content']
+    if request.method == 'POST':
+        task.content = request.form['content']
 
         try:
             db.session.commit()
@@ -59,10 +59,10 @@ def update(id):
 
         except:
             return "There was an issue updating your task"
-            
-    else:
-        return render_template('update.html',task=task)
-   
 
-if (__name__)=="main":
+    else:
+        return render_template('update.html', task=task)
+
+
+if (__name__) == "main":
     app.run(debug=True)
